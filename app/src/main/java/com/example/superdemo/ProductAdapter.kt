10ff11e26d86a1ba658e244031a8f9c61d73f630
mullatoez <kimanthikevin76@gmail.com>
2021-05-19 13:15:49 +0300

    super-demo

--- app/src/main/java/com/example/superdemo/ProductAdapter.kt
+++ app/src/main/java/com/example/superdemo/ProductAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
@@ -35,7 +36,7 @@ class ProductAdapter : RecyclerView.Adapter<ProductCustomViewHolder>() {
 
     }
 
-    fun submitList(productList: List<ProductModel>){
+    fun submitList(productList: List<ProductModel>) {
         items = productList
     }
 }
@@ -46,5 +47,15 @@ class ProductCustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
     val productDesc: TextView = itemView.findViewById(R.id.textView_product_description)
     val productInStock: TextView = itemView.findViewById(R.id.textView_in_stock)
     val productImage: ImageView = itemView.findViewById(R.id.imageView_product)
+
+    init {
+        itemView.setOnClickListener {
+            Toast.makeText(
+                itemView.context,
+                "Ooh sorry. The feature isn't available for now", Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+    }
 
 }
