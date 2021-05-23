1e3903da9486e8a209ad0234c9c70cbe359fa8de
mullatoez <kimanthikevin76@gmail.com>
2021-05-23 14:00:56 +0300

    login-page

--- app/src/main/java/com/example/superdemo/LoginActivity.kt
+++ app/src/main/java/com/example/superdemo/LoginActivity.kt
@@ -1,38 +1,32 @@
 package com.example.superdemo
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.example.superdemo.databinding.ActivityRegisterBinding
+import com.example.superdemo.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonRegister.setOnClickListener {
+        binding.buttonLoginUser.setOnClickListener {
 
-            performUserRegistration()
+            performUserLogin()
 
         }
-
-        binding.textViewAlreadyHaveAccountRegister.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 
-    private fun performUserRegistration() {
+    private fun performUserLogin() {
 
-        val email = binding.editTextTextEmailRegister.text.toString()
-        val password = binding.editTextTextPasswordRegister.text.toString()
+        val email = binding.editTextTextEmailLogin.text.toString()
+        val password = binding.editTextTextPasswordLogin.text.toString()
 
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT)
@@ -40,10 +34,10 @@ class RegisterActivity : AppCompatActivity() {
             return
         }
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if (it.isSuccessful) {
-                    Toast.makeText(this, "User registered successful", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "User logged in successful", Toast.LENGTH_SHORT)
                         .show()
 
                     val intent = Intent(this, MainActivity::class.java)
